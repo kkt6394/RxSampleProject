e84e049edc32edbc0051a58a283ff86088a4a0f2
kkt6394 <kkt6394@gmail.com>
2026-02-20 18:16:30 +0900

    NumbersViewController 구현

---
+++ RxSampleProject/NumbersViewController.swift
@@ -0,0 +1,78 @@
+//
+//  NumbersViewController.swift
+//  RxSampleProject
+//
+//  Created by 김기태 on 2/20/26.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class NumbersViewController: UIViewController {
+
+    private let disposeBag = DisposeBag()
+    
+    private let number1 = UITextField()
+    private let number2 = UITextField()
+    private let number3 = UITextField()
+
+    private let result = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        configureHierarchy()
+        configureLayout()
+        configureView()
+        rxTest()
+    }
+    
+    private func configureHierarchy() {
+        [number1, number2, number3, result]
+            .forEach { view.addSubview($0) }
+    }
+    private func configureLayout() {
+        number1.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(50)
+            make.height.equalTo(80)
+        }
+        
+        number2.snp.makeConstraints { make in
+            make.top.equalTo(number1.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(50)
+            make.height.equalTo(80)
+        }
+        number3.snp.makeConstraints { make in
+            make.top.equalTo(number2.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(50)
+            make.height.equalTo(80)
+        }
+        result.snp.makeConstraints { make in
+            make.top.equalTo(number3.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(80)
+        }
+    }
+    
+    private func configureView() {
+        number1.borderStyle = .bezel
+        number2.borderStyle = .bezel
+        number3.borderStyle = .bezel
+
+    }
+    
+    private func rxTest() {
+        Observable.combineLatest(number1.rx.text.orEmpty, number2.rx.text.orEmpty, number3.rx.text.orEmpty) { value1, value2, value3 -> Int in
+            return (Int(value1) ?? 0) + (Int(value2) ?? 0) + (Int(value3) ?? 0)
+        }
+        .map(\.description)
+        .bind(to: result.rx.text)
+        .disposed(by: disposeBag)
+    }
+}
